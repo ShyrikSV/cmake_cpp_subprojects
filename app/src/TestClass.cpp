bca1d9a87b38c7e3b6e5ae60fc81d4f39fad5902
Alexandr <em4info@protonmail.com>
2020-08-15 18:26:26 +0300

    Add simple app and folders structure

---
+++ app/src/TestClass.cpp
@@ -0,0 +1,7 @@
+#include "TestClass.hpp"
+#include <iostream>
+
+void TestClass::test_out()
+{
+    std::cout << "test_out\n";
+}
---
+++ app/src/main.cpp
@@ -0,0 +1,8 @@
+#include "TestClass.hpp"
+
+int main(int argc, char** argv)
+{
+    TestClass tc;
+    tc.test_out();
+    return 0;
+}
